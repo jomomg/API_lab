a57a4c45ad3aee89a9213409f0c5eac7ce66c159
Jomo <jomomg@gmail.com>
2017-02-08 18:41:56 +0300

    Added the ability to take multiple words as arguments and return their respective definitions

--- console_dictionary.py
+++ console_dictionary.py
@@ -5,34 +5,35 @@ import argparse
    and return the definition for a given word."""
 
 parser = argparse.ArgumentParser(
-    description="A Simple Command Line Dictionary Program")
-parser.add_argument('word', type=str, help='enter a word to get the definition')
+    description="A Simple Command Line Dictionary Program",
+    usage="console_dictionary.py word1 word2 word3 ...")
+parser.add_argument('word', nargs='*', type=str, help='enter words to get definitions')
 
 args = parser.parse_args()
 api_url = 'http://api.pearson.com/v2/dictionaries/ldoce5/entries'
 
+for word in args.word:
+    payload = {'headword': word}
+    try:
+        response = requests.get(api_url, params=payload)
+        response.raise_for_status()
+    except ConnectionError:
+        print("Could not establish a connection")
+        exit(2)
+    except TimeoutError:
+        print("The request timed out")
+        exit(3)
 
-payload = {'headword': args.word}
-try:
-    response = requests.get(api_url, params=payload)
-except ConnectionError:
-    print("Could not establish a connection")
-except TimeoutError:
-    print("The request timed out")
+    json_data = response.json()
+    r = json_data['results']
 
-json_data = response.json()
-
-r = json_data['results']
-print("Word: {}".format(args.word))
-for i in range(0, len(r)):
-    if r[i]['headword'] == args.word:
-        print("Part of speech: {}".format(r[i]['part_of_speech']))
-
-        print("Definition:")
-        print(r[i]['senses'][0]['definition'])
-        print("\n")
-
-
+    print("Word: {}".format(word))
+    for i in range(0, len(r)):
+        if word == r[i]['headword']:
+            print("Part of speech: {}".format(r[i]['part_of_speech']))
+            print("Definition:")
+            print(r[i]['senses'][0]['definition'])
+            print("\n")
 
 
 
